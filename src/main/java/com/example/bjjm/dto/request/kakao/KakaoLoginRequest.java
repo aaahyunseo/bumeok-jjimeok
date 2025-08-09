b705bab14c4d2f9addce62f8ffaf9ef4b0e1eab6
aaahyunseo <hs030608@naver.com>
2025-08-09 17:33:06 +0900

    feat : 카카오 로그인 Request Dto 추가 - #5

---
+++ src/main/java/com/example/bjjm/dto/request/kakao/KakaoLoginRequest.java
@@ -0,0 +1,10 @@
+package com.example.bjjm.dto.request.kakao;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class KakaoLoginRequest {
+    private String accessToken;
+}
