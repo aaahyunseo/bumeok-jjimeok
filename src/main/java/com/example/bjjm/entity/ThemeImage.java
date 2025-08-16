ab876f432e0c1cf2b9c5163f0b34b9b23f6805c8
aaahyunseo <hs030608@naver.com>
2025-08-16 15:38:44 +0900

    feat : 테마 관련 Entity 수정 - #9

---
+++ src/main/java/com/example/bjjm/entity/ThemeImage.java
@@ -0,0 +1,26 @@
+package com.example.bjjm.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "theme_images")
+public class ThemeImage extends BaseEntity {
+    @Column
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "theme_item_id")
+    private ThemeItem themeItem;
+}
