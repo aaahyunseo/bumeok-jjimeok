9b33a315c814189a0fd6ccd3191308e3c1a110e0
aaahyunseo <hs030608@naver.com>
2025-08-21 22:15:21 +0900

    feat : ErrorCode 추가 - #15

--- src/main/java/com/example/bjjm/exception/errorcode/ErrorCode.java
+++ src/main/java/com/example/bjjm/exception/errorcode/ErrorCode.java
@@ -21,6 +21,9 @@ public enum ErrorCode {
     THEME_NOT_FOUND("4042", "테마를 찾을 수 없습니다."),
     THEME_COMMENT_NOT_FOUND("4043", "테마 댓글을 찾을 수 없습니다."),
     THEME_SCRAP_NOT_FOUND("4044", "스크랩한 테마가 아닙니다."),
+    PUZZLE_NOT_FOUND("4047", "퍼즐 여부를 찾을 수 없습니다."),
+    MISSION_NOT_FOUND("4048", "상세 미션을 찾을 수 없습니다."),
+    PLACE_NOT_FOUND("4049", "장소의 위치 인증에 실패했습니다."),
 
     // ConflictException
     THEME_SCRAP_CONFLICT("4090", "이미 스크랩한 테마입니다."),
