0c779a3238a355c3096a745786dca6284c841e8b
aaahyunseo <hs030608@naver.com>
2025-08-22 17:45:54 +0900

    feat : MissionRepository 수정 - #15

--- src/main/java/com/example/bjjm/repository/MissionRepository.java
+++ src/main/java/com/example/bjjm/repository/MissionRepository.java
@@ -1,12 +1,12 @@
 package com.example.bjjm.repository;
 
 import com.example.bjjm.entity.Mission;
-import com.example.bjjm.entity.PuzzleRegion;
+import com.example.bjjm.entity.Puzzle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface MissionRepository extends JpaRepository<Mission, UUID> {
-    List<Mission> findAllByPuzzleRegion (PuzzleRegion puzzleRegion);
+    List<Mission> findAllByPuzzle (Puzzle puzzle);
 }
