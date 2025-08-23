67452b0554991f483fd8040c9f97e2686f9287db
aaahyunseo <hs030608@naver.com>
2025-08-23 18:17:51 +0900

    feat : PuzzleRepository 메서드 수정 - #15

--- src/main/java/com/example/bjjm/repository/PuzzleRepository.java
+++ src/main/java/com/example/bjjm/repository/PuzzleRepository.java
@@ -1,12 +1,11 @@
 package com.example.bjjm.repository;
 
 import com.example.bjjm.entity.Puzzle;
-import com.example.bjjm.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PuzzleRepository extends JpaRepository<Puzzle, UUID> {
-    List<Puzzle> findAllByUser(User user);
+    Optional<Puzzle> findById(UUID puzzleId);
 }
