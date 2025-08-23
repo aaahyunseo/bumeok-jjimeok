d21cb8e50ceaa26d132757a24303167b2d50458e
aaahyunseo <hs030608@naver.com>
2025-08-23 18:30:41 +0900

    feat : Mission setter 추가 - #15

--- src/main/java/com/example/bjjm/entity/Mission.java
+++ src/main/java/com/example/bjjm/entity/Mission.java
@@ -1,10 +1,7 @@
 package com.example.bjjm.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 
@@ -13,6 +10,7 @@ import java.util.List;
 @NoArgsConstructor
 @Entity
 @Getter
+@Setter
 @Table(name = "missions")
 public class Mission extends BaseEntity {
 
