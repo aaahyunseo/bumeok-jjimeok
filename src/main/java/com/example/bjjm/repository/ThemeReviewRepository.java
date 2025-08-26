96373c1fd19690f2384e60eaa9fb16c0a53944e9
aaahyunseo <hs030608@naver.com>
2025-08-26 18:01:26 +0900

    feat : ThemeReviewRepository 메서드 추가 - #15

--- src/main/java/com/example/bjjm/repository/ThemeReviewRepository.java
+++ src/main/java/com/example/bjjm/repository/ThemeReviewRepository.java
@@ -2,6 +2,7 @@ package com.example.bjjm.repository;
 
 import com.example.bjjm.entity.Theme;
 import com.example.bjjm.entity.ThemeReview;
+import com.example.bjjm.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,4 +10,5 @@ import java.util.UUID;
 
 public interface ThemeReviewRepository extends JpaRepository<ThemeReview, UUID> {
     List<ThemeReview> findAllByTheme(Theme theme);
+    int countByUser(User user);
 }
