1e0763a8b75af6a4f9e04eed6c608bc0596e7ad4
aaahyunseo <hs030608@naver.com>
2025-08-27 22:11:37 +0900

    feat : UserService 유저 정보 반환 로직 추가 - #19

--- src/main/java/com/example/bjjm/service/UserService.java
+++ src/main/java/com/example/bjjm/service/UserService.java
@@ -1,10 +1,33 @@
 package com.example.bjjm.service;
 
+import com.example.bjjm.dto.response.user.UserInfoResponseDto;
+import com.example.bjjm.entity.User;
+import com.example.bjjm.entity.UserPuzzle;
+import com.example.bjjm.repository.UserPuzzleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+    private final UserPuzzleRepository userPuzzleRepository;
 
+    public UserInfoResponseDto getUserInfo(User user) {
+        List<UserPuzzle> userPuzzles = userPuzzleRepository.findAllByUser(user);
+
+        int collectedPuzzleCount = 0;
+        int completedMissionCount = 0;
+
+        for (UserPuzzle userPuzzle : userPuzzles) {
+            // 유저의 퍼즐 완료 여부
+            if (userPuzzle.getPuzzleCompleted()) collectedPuzzleCount++;
+
+            // 유저가 완료한 미션 수
+            completedMissionCount += userPuzzle.getCollectedMissionCount();
+        }
+
+        return UserInfoResponseDto.of(user, completedMissionCount, collectedPuzzleCount);
+    }
 }
