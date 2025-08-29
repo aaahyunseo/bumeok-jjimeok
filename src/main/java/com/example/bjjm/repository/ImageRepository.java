f076c793b221345d6f9571c6081e945656cdea1c
aaahyunseo <hs030608@naver.com>
2025-08-29 23:49:03 +0900

    feat : 사용되지 않는 클래스 삭제 - #23

--- src/main/java/com/example/bjjm/repository/ImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.bjjm.repository;
-
-import com.example.bjjm.entity.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface ImageRepository extends JpaRepository<Image, UUID> {
-}
