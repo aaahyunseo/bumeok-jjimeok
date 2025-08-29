9524c2bd8ec24477d41c2304aeb8a4bf7d22a774
aaahyunseo <hs030608@naver.com>
2025-08-29 23:50:14 +0900

    feat : repository 메서드 수정 - #23

--- src/main/java/com/example/bjjm/repository/PlaceReviewRepository.java
+++ src/main/java/com/example/bjjm/repository/PlaceReviewRepository.java
@@ -4,7 +4,6 @@ import com.example.bjjm.entity.PlaceReview;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 public interface PlaceReviewRepository extends JpaRepository<PlaceReview, UUID> {
