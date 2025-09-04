9087a922b55a6aa89cf297e94e45aeb4fa1d87a1
aaahyunseo <hs030608@naver.com>
2025-09-04 20:46:21 +0900

    feat : AuthenticationConfig 수정 - #31

--- src/main/java/com/example/bjjm/configuration/AuthenticationConfig.java
+++ src/main/java/com/example/bjjm/configuration/AuthenticationConfig.java
@@ -27,7 +27,7 @@ public class AuthenticationConfig implements WebMvcConfigurer {
                                 "/home")
                 .excludePathPatterns("/test", "/home/top3", "/home/theme/**", "/map", "/place-reviews/list", "/api/tour/**",
                         "/puzzles/mission/**", "/puzzles/ranking",
-                        "/themes/list/**", "/themes/keyword/**", "/themes/*/detail", "/themes/*/comment/list" , "/themes/*/review/list");
+                        "/themes/list/**", "/themes/keyword/**", "/themes/*/comment/list" , "/themes/*/review/list");
     }
 
     @Override
